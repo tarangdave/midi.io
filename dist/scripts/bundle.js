c2ac34f8d81debc9c23d42dca69b9f76ff6a5a01
Tarang Dave <tarangadave@gmail.com>
2016-07-28 13:06:26 +0530

    added key stroke support in react

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -20424,6 +20424,39 @@ module.exports = require('./lib/React');
 var React = require('react');
 
 var Plate = React.createClass({displayName: "Plate",
+    componentDidMount: function() {
+        $(document).keypress(function(e){
+			if(e.which == 55 || e.which == 116){
+				console.log("pressed 7 button");
+			}
+			if(e.which == 56 || e.which == 121){
+				console.log("pressed 8 button");
+			}
+			if(e.which == 57 || e.which == 117){
+				console.log("pressed 9 button");
+			}
+			if(e.which == 52 || e.which == 103){
+				console.log("pressed 4 button");
+			}
+			if(e.which == 53 || e.which == 104){
+				console.log("pressed 5 button");
+			}
+			if(e.which == 54 || e.which == 106){
+				console.log("pressed 6 button");
+			}
+			if(e.which == 49 || e.which == 118){
+				console.log("pressed 1 button");
+			}
+			if(e.which == 50 || e.which == 98){
+				console.log("pressed 2 button");
+			}
+			if(e.which == 51 || e.which == 110){
+				console.log("pressed 3 button");
+			}
+			
+		});
+    },
+
     render: function(){
         return (
         React.createElement("div", null, 
